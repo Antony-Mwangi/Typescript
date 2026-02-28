e0aedfed3bc80334ea2ab685f465824d0f8a4f92
Antony Mwangi <antonymwangiw85@gmail.com>
2026-02-28 14:34:32 -0500

    boolean datatype

---
+++ simpleTypes.ts
@@ -0,0 +1,12 @@
+// This program demonstrates Typescript primitive types: boolean, number, string, bigint, and symbol.
+
+// BOOLEAN
+//Used for true/false values.
+
+//Use account status (true=active, false=inactive)
+
+let isActive: boolean = true;
+let hasPermission: boolean = false;
+
+console.log("Account Active:", isActive);
+console.log("Has Permission:", hasPermission);
